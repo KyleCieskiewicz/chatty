5deb354c82ab2fdbd53031db15ca893eca8281ad
kyle cieskiewicz <kylecieskiewicz@kyles-MacBook-Pro.local>
2017-09-19 16:25:55 -0500

    chatty

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+let messages = ['hi', 'bye', 'hello world'];
+
+app.get('/messages', function(req, res, next){
+    res.status(200).json({messages: messages});
+});
+
+app.post('/messages', function (req, res, next) {
+    messages.push({ message: req.body.message, time: new Date() });
+    res.status(200).json({ messages: messages });
+  });
+
+  app.listen(3000, () => {
+	console.log('running on port 3000');
+})
